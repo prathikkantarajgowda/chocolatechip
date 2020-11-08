ca26cb321e5d53df6f4709e6d875665647b16abf
prathikgowda <gowdapra@grinnell.edu>
2020-11-08 18:18:37 +0000

    Added fontset declaration, altered cpu_t decleration, added function prototypes and more

--- chocolatechip.c
+++ chocolatechip.c
@@ -3,3 +3,30 @@
 
 #include "chip8.h"
 
+int
+main(int argc, char **argv)
+{
+	if (argc != 2) {
+		printf("One ROM file needed. Usage: chocolatechip rom.ch8\n");
+		return 1;
+	}
+
+}
+
+void
+load_fontset(cpu_t cpu)
+{
+	for (int i = 0; i < 80; i++)
+		cpu->memory[i]
+}
+	
+int
+load_rom(cpu_t cpu, char *romfile)
+{
+	if (!(cpu->rom = fopen(romfile, "rb"))) {
+		printf("Invalid ROM filename: %s", romfile);
+		exit(1);
+	}
+
+	/* Load ROM into memory here */
+}
