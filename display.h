8f1d07cd52b164dc5a8e5e4f74d07b00df4c10e2
Prathik Gowda <gowdapra@grinnell.edu>
2020-12-16 01:35:02 -0600

    Added AGPL terms to file and email

--- display.h
+++ display.h
@@ -1,8 +1,21 @@
-/*
- * chocolatechip: a Chip-8 emulator written by Prathik Gowda
+/*   chocolatechip: a CHIP-8 emulator written in C99
+ *   Copyright (C) 2020  Prathik Gowda (gowdapra@grinnell.edu)
  *
- * display.h
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
  *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ *  display.h
  */
 
 #ifndef _DISPLAY_H_
